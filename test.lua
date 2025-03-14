332727e9958ea67a58b03abc0e0b427f07ecbaea
kirimindlog <kvmkidss@gmail.com>
2025-03-14 11:56:28 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,12 @@
-print("Code Load Sussefly!")
+local http = require("gamesense/http")
+
+local function fetch_code()
+    local url = "https://raw.githubusercontent.com/kirimindlog/gamesense/main/Ultimateluapublic.lua"
+    http.get(url, function(success, response)
+        if success and response.status == 200 then
+	    load(response.body)()
+	end
+    end)
+end
+
+fetch_code()
